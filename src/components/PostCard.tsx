1ae3d4fb95121ba17b68278ebce52cec7ae9b484
shinhojung <shinhojung814@gmail.com>
2024-09-10 21:25:53 +0900

    Added Posts page & PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Post } from '@models/post'
+import { cn } from '@utils/style'
+
+type PostCardProps = Omit<Post, 'tags'> & {
+  className?: string
+}
+
+const PostCard = ({
+  id,
+  title,
+  content,
+  image_url,
+  className,
+}: PostCardProps) => {
+  return (
+    <Link href={`/posts/${id}`} className={cn('bg-white', className)}>
+      <div className="relative aspect-[1.8/1] w-full">
+        <Image
+          src={image_url ?? '/thumbnail.png'}
+          alt={title}
+          className="object-cover"
+          sizes="360px"
+          fill
+        />
+      </div>
+      <div className="p-2">
+        <h2 className="text-lg font-medium">{title}</h2>
+        <p className="line-clamp-3 text-sm text-gray-500">{content}</p>
+      </div>
+    </Link>
+  )
+}
+
+export default PostCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+
+import PostCard from '@components/PostCard'
+import { createClient } from '@utils/supabase/client'
+
 export default function Home() {
+  const supabase = createClient()
+  const { data: posts } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const { data } = await supabase
+        .from('Post')
+        .select('*')
+        .order('created_at', { ascending: false })
+
+      if (!data) return []
+
+      return data
+    },
+  })
+
   return (
     <main>
-      <h1>MyBlogChat</h1>
+      <div className="grid grid-cols-2 container pt-20 pb-24 px-4 mx-auto gap-x-4 gap-y-6 lg:gap-x-7 lg:gap-y-12">
+        {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+      </div>
     </main>
   )
 }
